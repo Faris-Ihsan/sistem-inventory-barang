a60b47dec6b56d925942d3a30d01fc871e5dd181
Faris-Ihsan <farisihsan32@yahoo.co.id>
2022-12-21 12:01:22 +0700

    route into index page

--- main/routes.py
+++ main/routes.py
@@ -3,7 +3,7 @@ import os
 from flask import request, redirect, render_template
 
 # a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
